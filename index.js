213b1e0d13f45ddbc9e12cff5179d3566fcc1231
Jennifer Ridgeway <jennifrmarie@icloud.com>
2019-09-08 20:14:28 -0400

    second

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-// function for landing page to listen to user submit
-// function that listens for user to click
-// function that determines the number the user inputs and 
-// function that displays results based on number input
-
-
 'use strict';
-
-// function getDogImage() {
-//     let urlAdd = $("#text").val();
-//     let fetchPromise = fetch("https://dog.ceo/api/breeds/image/random/" + urlAdd);
-//     console.log(fetchPromise)
-//     const userInput = document.getElementById("text");
-//     fetchPromise.then(response => {
-//         return response.json();
-//     }).then(url => {
-//         displayResults(url);
-//     })
-//     // userInput.innerHTML = url;
-//     // fetchPromise.then(results => {
-//         // displayResults(responseJson);
-//     // })
-
-// }
 
 function getDogImage() {
     let url = "https://dog.ceo/api/breeds/image/random/" + $('#text').val();
